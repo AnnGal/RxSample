ce7adad61ca2332b8b5f8a778e4057e9c224f86d
Anya Galyamova <galyamova.anya@gmail.com>
2020-11-04 21:59:14 +0300

    feat: add Flowable, Single, Maybe, Completable

--- app/src/main/java/an/maguste/android/rxsample/MainActivity.kt
+++ app/src/main/java/an/maguste/android/rxsample/MainActivity.kt
@@ -6,9 +6,7 @@ import android.widget.Toast
 import androidx.annotation.MainThread
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Flowable
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.core.*
 import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -24,10 +22,10 @@ class MainActivity : AppCompatActivity() {
             logAndAppend("Observable new data $it")
         }
 
-        val single = Single.just(1)
+        /*val single = Single.just(1)
         val disposeSingle = single.subscribe({
             logAndAppend("Single new data $it")
-        }, {})
+        }, {})*/
 
         val flowable = Flowable.just(1, 2, 3, 4, 5)
         val disposeFlowable = flowable.subscribe {
@@ -40,7 +38,7 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,"Button clicked", Toast.LENGTH_SHORT )
         }
 
-        val dispose = dataSource()
+        val dispose = dataSourceFlowable()
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
@@ -52,16 +50,76 @@ class MainActivity : AppCompatActivity() {
 
                 })
 
+        val disposeSingle = dataSourceSingle()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    logAndAppend("Singe ${it.joinToString { "$it#" }}")
+                },{
+
+                })
+
+        val disposeCompletable = dataSourceCompletable()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    logAndAppend("Completable success")
+                },{
+                    logAndAppend("Completable failure")
+                })
+
+        val disposeMaybe = dataSourceMaybe()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    logAndAppend("Maybe success $it")
+                },{
+                    logAndAppend("Maybe failure")
+                },{
+                    logAndAppend("Maybe complete")
+                })
     }
 
-    fun dataSource(): Observable<Int> {
+    fun dataSourceObservable(): Observable<Int> {
         return Observable.create { subscriber ->
             for (i in 0..100 step 5){
-                Thread.sleep(100)
+                Thread.sleep(10000)
                 subscriber.onNext(i)
             }
         }
     }
+
+    // Flowable almost the same as Observable
+    fun dataSourceFlowable(): Flowable<Int> {
+        return Flowable.create ({ subscriber ->
+            for (i in 0..50 step 10){
+                subscriber.onNext(i)
+            }
+            subscriber.onComplete()
+        }, BackpressureStrategy.BUFFER) //, DROP, LATEST
+    }
+
+    fun dataSourceSingle(): Single<List<Int>> {
+        return Single.create { subscriber ->
+            val list = listOf(0,1,2,3,4,5,6,7,8,9,10)
+            subscriber.onSuccess(list)
+        }
+    }
+
+    fun dataSourceCompletable(): Completable {
+        return Completable.create { subscriber ->
+            subscriber.onComplete()
+        }
+    }
+
+    fun dataSourceMaybe(): Maybe<Int> {
+        return Maybe.create  { subscriber ->
+            val list = listOf(0,1,2,3,4,5,6,7,8,9,10)
+            subscriber.onSuccess(list.filter{ it % 2 != 0 }.sum())
+            subscriber.onComplete()
+        }
+    }
+
 
     private fun logAndAppend(text: String){
         textLog.append("$text \n")
